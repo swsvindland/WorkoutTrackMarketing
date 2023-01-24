1649b1ce894151c8f01199321f83882e5c9418ca
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-01-24 10:34:43 -0500

    add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,9 @@
 import '@/styles/tailwind.css';
 import 'focus-visible';
 import { Oswald } from '@next/font/google';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { firebaseConfig } from '@/utils/firebase';
-import { getApps, initializeApp } from 'firebase/app';
-import { getAnalytics, isSupported } from 'firebase/analytics';
-
-if (getApps().length === 0) {
-  initializeApp(firebaseConfig);
-}
+import * as ReactGA from 'react-ga';
 
 const oswald = Oswald({
   subsets: ['latin'],
@@ -17,29 +11,14 @@ const oswald = Oswald({
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const [tracking, setTracking] = useState(null);
 
   useEffect(() => {
-    if (isSupported() === false) return;
-    setTracking(getAnalytics());
+    ReactGA.initialize('G-6Y0BKEDD3H');
+  }, []);
 
-    const handleRouteChange = (url) => {
-      if (!tracking) {
-        return;
-      }
-
-      tracking.logEvent('page_view', {
-        page_location: url,
-        page_title: document?.title,
-      });
-    };
-
-    router.events.on('routeChangeStart', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, [router.events, setTracking, tracking]);
+  useEffect(() => {
+    ReactGA.pageview(router.pathname);
+  }, [router.pathname]);
 
   return (
     <div className={oswald.className}>
